12644aca8b869058c6987482a62551b224ee11e5
DioCrafts <pozo.dionisio@gmail.com>
2025-04-27 23:42:20 +0200

    adding features

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.android.tools.build:gradle:8.7.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
@@ -17,7 +17,7 @@ allprojects {
 }
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
