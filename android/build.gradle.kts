ee2170f60e0494ac4d62078b357e733d7661ed15
DioCrafts <pozo.dionisio@gmail.com>
2025-04-28 00:20:03 +0200

    adding features

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("dev.flutter.flutter-gradle-plugin:1.0.0")
     }
 }
 
@@ -19,7 +18,7 @@ allprojects {
 
 plugins {
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
